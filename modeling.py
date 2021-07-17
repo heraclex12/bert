784d952d5f8118eeabe9c0ff0c689880fba16277
Hieu Trung Tran <heraclex12@gmail.com>
2021-07-17 16:13:13 +0700

    change decoder->encoder var name

--- modeling.py
+++ modeling.py
@@ -354,7 +354,7 @@ class BertDecoder(object):
             max_position_embeddings=config.max_position_embeddings,
             dropout_prob=config.hidden_dropout_prob)
 
-      with tf.variable_scope("decoder"):
+      with tf.variable_scope("encoder"):
         # This converts a 2D mask of shape [batch_size, seq_length] to a 3D
         # mask of shape [batch_size, seq_length, seq_length] which is used
         # for the attention scores.
